350dc4e5e61590f37c88d4adead827e1cab9325e
Ankur Goel <ankurgel@gmail.com>
2012-04-08 19:24:22 +0530

    Code indented and output updated

--- code.rb
+++ code.rb
@@ -1,21 +1,21 @@
 def revise_arr(l)
-	help_words=%w(are am is was were has have must shall could would should couldn't shouldn't musn't wouldn't may might may can can't will)
-	help_words=help_words.join(' ').upcase.split; match_found=false
-	help_words.each do |x|
-	if l.include?(x)&&(l.index(x))!=0
-	then
-	i=l.index(x)-1
-	l[i]+=" "+l[i+1];l.delete_at(i+1); match_found=true
-	end
-	break if match_found==true
-	end
-	help_words.each{|x| if (match_found==false)&&(l[0]==x) then l[0]+=" "+l[1]; l.delete_at(1); end}
-	if l.include?('A') then i=l.index('A'); l[i]+=" "+l[i+1]; l.delete_at(i+1); end
-	l
+   help_words=%w(are am is was were has have must shall could would should couldn't shouldn't musn't wouldn't may might may can can't will)
+   help_words=help_words.join(' ').upcase.split; match_found=false
+   help_words.each do |x|
+      if l.include?(x)&&(l.index(x))!=0
+         then
+         i=l.index(x)-1
+         l[i]+=" "+l[i+1];l.delete_at(i+1); match_found=true
+      end
+      break if match_found==true
+   end
+   help_words.each{|x| if (match_found==false)&&(l[0]==x) then l[0]+=" "+l[1]; l.delete_at(1); end}
+   if l.include?('A') then i=l.index('A'); l[i]+=" "+l[i+1]; l.delete_at(i+1); end
+   l
 end
 file=File.open('lines_db')
 file.each do |l|
-l=l.upcase.gsub(/[^A-Z' ]/,'').split
-rev=revise_arr(l)
-puts rev.shuffle.join(' ')
+   l=l.upcase.gsub(/[^A-Z' ]/,'').split
+   rev=revise_arr(l)
+   puts rev.shuffle.join(' ')
 end
